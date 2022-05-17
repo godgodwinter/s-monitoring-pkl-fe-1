4cd5b5ef31f232b9a5352ae61e9ed11b90a87c08
godgodwinter <kakadz.pro@gmail.com>
2022-05-17 23:34:54 +0700

    update some component

--- src/views/admin/absensi/Index.vue
+++ src/views/admin/absensi/Index.vue
@@ -39,6 +39,12 @@ for (let i = 0; i < daysInMonth; i++) {
   });
 }
 console.log(data.value);
+const dataDetail = ref({
+  monthyear: {
+    month: month,
+    year: year,
+  },
+});
 </script>
 <template>
   <!-- Modal -->
@@ -116,7 +122,97 @@ console.log(data.value);
   </div>
   <div class="py-2 px-2">
     <div class="w-full bg-white shadow-sm py-4 rounded-sm px-4 flex justify-center">
-      <h1 class="font-semibold text-xl text-gray-700">Absensi</h1>
+      <div class="py-2">
+        <h1 class="font-semibold text-xl text-gray-700 text-center">
+          Data Absensi Siswa
+        </h1>
+        <div class="flex justify-center">
+          <div>
+            <div class="py-2">
+              <Datepicker
+                format="yyyy/MM"
+                value-format="yyyy-MM"
+                v-model="dataDetail.monthyear"
+                monthPicker
+                required
+              >
+                <template #calendar-header="{ index, day }">
+                  <div :class="index === 5 || index === 6 ? 'red-color' : ''">
+                    {{ day }}
+                  </div>
+                </template>
+              </Datepicker>
+            </div>
+            <div class="py-2 space-x-2">
+              <button
+                @click="openModal"
+                type="button"
+                data-modal-toggle="user-modal"
+                class="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center"
+              >
+                <svg
+                  class="mr-2 h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+                  ></path>
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Absen
+              </button>
+
+              <button
+                type="button"
+                data-modal-toggle="user-modal"
+                class="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center"
+              >
+                <svg
+                  class="mr-2 h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+                  ></path>
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Isi Jurnal
+              </button>
+              <button
+                type="button"
+                data-modal-toggle="delete-user-modal"
+                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2 text-center"
+              >
+                <svg
+                  class="mr-2 h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Batalkan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -240,7 +336,7 @@ console.log(data.value);
 
                   <td class="p-4 whitespace-nowrap space-x-2">
                     <div v-if="item.id == dateNow" class="space-x-2">
-                      <button
+                      <!-- <button
                         @click="openModal"
                         type="button"
                         data-modal-toggle="user-modal"
@@ -304,7 +400,7 @@ console.log(data.value);
                           ></path>
                         </svg>
                         Batalkan
-                      </button>
+                      </button> -->
                     </div>
                   </td>
                 </tr>
